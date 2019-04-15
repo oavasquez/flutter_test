3e5edc282abc5c10e0a2d97cde6b453860e16f37
AndresVasquez <oavasquez01@gmail.com>
2019-04-14 23:20:01 -0600

    guardar datos temporales en la app (url del servidor)

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:testapp/layout.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() => runApp(new MyApp());
 
@@ -30,24 +31,29 @@ class MyApp extends StatelessWidget {
 }
 
 class Login extends StatelessWidget {
-
-
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('First Route'),
+        title: Text('Login'),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.settings),
+            tooltip: 'Configurar parametros de conexion',
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  new MaterialPageRoute(
+                      builder: (context) => new ConfigurationPage()));
+            },
+          ),
+        ],
       ),
-      body: Center(
-          child: LoginForm()
-      ),
+      body: Center(child: LoginForm()),
     );
   }
 }
-
-
 
 class LoginForm extends StatefulWidget {
   @override
@@ -68,59 +74,76 @@ class LoginFormState extends State<LoginForm> {
   @override
   Widget build(BuildContext context) {
     // Build a Form widget using the _formKey we created above
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          TextFormField(
-            decoration: InputDecoration(
-                labelText: 'Nombre de Usuario'
-            ),
-            /*
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Por favor ingrese un usuario valido';
-              }
-            },*/
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-                labelText: 'Contraseña'
-            ),
-            /*
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Por favor ingrese una contraseña valida';
-              }
-            },*/
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: RaisedButton(
-              onPressed: () {
-                // Validate will return true if the form is valid, or false if
-                // the form is invalid.
-                if (_formKey.currentState.validate()) {
-                  // If the form is valid, we want to show a Snackbar
-                  /*Scaffold.of(context)
+    return Container(
+        alignment: Alignment(0.0, 0.0),
+        constraints: BoxConstraints.expand(width: 290.0, height: 300.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Nombre de Usuario',
+                  border: OutlineInputBorder(),
+                  icon: Icon(Icons.person),
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Por favor ingrese un usuario valido';
+                  }
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Contraseña',
+                    border: OutlineInputBorder(),
+                    icon: Icon(Icons.lock),
+                  ),
+                  obscureText: true,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Por favor ingrese una contraseña valida';
+                    }
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: ButtonTheme(
+                  minWidth: 200.0,
+                  height: 50.0,
+                  child: RaisedButton(
+                    textColor: Colors.white,
+                    padding: const EdgeInsets.all(8.0),
+                    color: Theme.of(context).accentColor,
+                    elevation: 4.0,
+                    splashColor: Colors.blueGrey,
+                    onPressed: () {
+                      // Validate will return true if the form is valid, or false if
+                      // the form is invalid.
+                      if (_formKey.currentState.validate()) {
+                        // If the form is valid, we want to show a Snackbar
+                        /*Scaffold.of(context)
                       .showSnackBar(SnackBar(content: Text('Processing Data')));*/
-                  Navigator.push(
-                      context, new MaterialPageRoute(
-                      builder: (context) => new MyHomePage(title: 'Aplicacion Demo')));
-                }
-
-              },
-              child: Text('Iniciar sesion'),
-            ),
+                        Navigator.push(
+                            context,
+                            new MaterialPageRoute(
+                                builder: (context) =>
+                                    new MyHomePage(title: 'Aplicacion Demo')));
+                      }
+                    },
+                    child: Text('Iniciar sesion'),
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
-    );
+        ));
   }
 }
-
-
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -131,13 +154,11 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
-
-
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   String _lastSelected = 'TAB: 0';
 
   int _selectedIndex = 1;
-  bool showFaloating =true;
+  bool showFaloating = true;
   final _widgetOptions = [
     new Container(
       color: Colors.amber.shade400,
@@ -148,7 +169,6 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
     Text('Index 2: School'),
     new BarCodePage(title: 'Aplicacion Demo'),
   ];
-
 
   void _onItemTapped(int index) {
     /*if (index==0) {
@@ -168,7 +188,6 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   }
 
   void _selectedFab(int index) {
-
     setState(() {
       _lastSelected = 'FAB: $index';
     });
@@ -204,7 +223,7 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   }
 
   Widget _buildFab(BuildContext context) {
-    final icons = [ Icons.local_grocery_store, Icons.note, Icons.phone];
+    final icons = [Icons.local_grocery_store, Icons.note, Icons.phone];
     return AnchoredOverlay(
       showOverlay: true,
       overlayBuilder: (context, offset) {
@@ -225,7 +244,6 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
     );
   }
 }
-
 
 class SecondScreen extends StatelessWidget {
   @override
@@ -248,23 +266,84 @@ class SecondScreen extends StatelessWidget {
   }
 }
 
-class SecondRoute extends StatelessWidget {
+class ConfigurationPage extends StatefulWidget {
+
+  @override
+  _ConfigurationPageState createState() => new _ConfigurationPageState();
+}
+
+class _ConfigurationPageState extends State<ConfigurationPage>  {
+
+  @override
+  void initState() {
+    super.initState();
+    getParametros();
+  }
+
+  TextEditingController _servidorController = new TextEditingController();
+  SharedPreferences sharedPreferences;
+
+
+  _conectarServidor (String servidor) async  {
+
+    sharedPreferences = await SharedPreferences.getInstance();
+    print(sharedPreferences.getString('servidor'));
+      await  sharedPreferences.setString('servidor', servidor);
+  }
+
+  getParametros() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+
+    _servidorController.text = sharedPreferences.getString("servidor");
+
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        title: Text("Configuracion de Conexion"),
+      ),
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            // Navigate back to first route when tapped.
-          },
-          child: Text('Go back!'),
-        ),
+        child: Flex(
+            direction: Axis.vertical,
+            mainAxisAlignment: MainAxisAlignment.center ,
+            children: <Widget>[
+
+                Text(
+                  'Ingrese Direccion del Servidor:',
+                  style: TextStyle(fontSize: 20),
+                  textAlign: TextAlign.center,
+
+                ),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Direccion IP',
+                    border: OutlineInputBorder(),
+
+
+                  ),
+                  keyboardType: TextInputType.url,
+
+                  controller: _servidorController,
+                ),
+              ),
+              RaisedButton(
+                onPressed: () {
+
+                  _conectarServidor ( _servidorController.text.toString());
+                },
+                child: Text("Sincronizar"),
+              ),
+            ]),
       ),
     );
   }
 }
-
 
 class BarCodePage extends StatefulWidget {
   BarCodePage({Key key, this.title}) : super(key: key);
@@ -275,8 +354,7 @@ class BarCodePage extends StatefulWidget {
   _BarCodePageState createState() => new _BarCodePageState();
 }
 
-class _BarCodePageState extends State<BarCodePage>  {
-
+class _BarCodePageState extends State<BarCodePage> {
   String _scanBarcode = 'Unknown';
 
   @override
@@ -290,7 +368,7 @@ class _BarCodePageState extends State<BarCodePage>  {
     // Platform messages may fail, so we use a try/catch PlatformException.
     try {
       barcodeScanRes =
-      await FlutterBarcodeScanner.scanBarcode("#ff6666", "Cancel", false);
+          await FlutterBarcodeScanner.scanBarcode("#ff6666", "Cancel", false);
     } on PlatformException {
       barcodeScanRes = 'Failed to get platform version.';
     }
@@ -308,13 +386,11 @@ class _BarCodePageState extends State<BarCodePage>  {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-
-        body: Center(
-            child:  Flex(
-                direction: Axis.vertical,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
+      body: Center(
+        child: Flex(
+            direction: Axis.vertical,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
               RaisedButton(
                 onPressed: () {
                   initPlatformState();
@@ -326,9 +402,7 @@ class _BarCodePageState extends State<BarCodePage>  {
                 style: TextStyle(fontSize: 20),
               ),
             ]),
-        ),
-
+      ),
     );
   }
-
 }
