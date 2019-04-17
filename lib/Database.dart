4b344b89f2dff79ef7890268939c66d714cb9afd
AndresVasquez <oavasquez01@gmail.com>
2019-04-17 00:09:28 -0600

    Intento para sincronizar la informacion del SQFLite con los datos obtenidos del servidor

---
+++ lib/Database.dart
@@ -0,0 +1,118 @@
+//https://github.com/Rahiche/sqlite_demo/blob/master/lib/main.dart
+
+import 'dart:async';
+import 'dart:io';
+
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:testapp/ClientModel.dart';
+import 'package:testapp/ArticuloModel.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DBProvider {
+  DBProvider._();
+
+  static final DBProvider db = DBProvider._();
+
+  Database _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database;
+    // if _database is null we instantiate it
+    _database = await initDB();
+    return _database;
+  }
+
+  initDB() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, "TestAPP.db");
+    return await openDatabase(path, version: 1, onOpen: (db) {},
+        onCreate: (Database db, int version) async {
+      await db.execute("CREATE TABLE ARTICULOS ("
+          "Id INTEGER PRIMARY KEY,"
+          "NombreArticulo TEXT"
+          ")");
+    });
+  }
+
+  newArticulo(Articulo newArticulo) async {
+    final db = await database;
+    //get the biggest id in the table
+    var table = await db.rawQuery("SELECT MAX(Id)+1 AS Id FROM ARTICULOS");
+
+    int id = table.first["Id"];
+    await print(newArticulo.nombreArticulo);
+    //insert to the table using the new id
+
+    var raw = await db.rawInsert(
+        "INSERT INTO ARTICULOS ( Id , NombreArticulo)"
+        " VALUES (?,?)",
+        [id, "prueba"]);
+    return raw;
+  }
+
+  blockOrUnblock(Client client) async {
+    final db = await database;
+    Client blocked = Client(
+        id: client.id,
+        firstName: client.firstName,
+        lastName: client.lastName,
+        blocked: !client.blocked);
+    var res = await db.update("Client", blocked.toMap(),
+        where: "id = ?", whereArgs: [client.id]);
+    return res;
+  }
+
+  updateClient(Client newClient) async {
+    final db = await database;
+    var res = await db.update("Client", newClient.toMap(),
+        where: "id = ?", whereArgs: [newClient.id]);
+    return res;
+  }
+
+  getClient(int id) async {
+    final db = await database;
+    var res = await db.query("Client", where: "id = ?", whereArgs: [id]);
+    return res.isNotEmpty ? Client.fromMap(res.first) : null;
+  }
+
+  Future<List<Client>> getBlockedClients() async {
+    final db = await database;
+
+    print("works");
+    // var res = await db.rawQuery("SELECT * FROM Client WHERE blocked=1");
+    var res = await db.query("Client", where: "blocked = ? ", whereArgs: [1]);
+
+    List<Client> list =
+        res.isNotEmpty ? res.map((c) => Client.fromMap(c)).toList() : [];
+    return list;
+  }
+
+  Future<List<Client>> getAllClients() async {
+    final db = await database;
+    var res = await db.query("Client");
+    List<Client> list =
+        res.isNotEmpty ? res.map((c) => Client.fromMap(c)).toList() : [];
+    return list;
+  }
+
+  Future<List<Articulo>> getAllArticulo() async {
+    final db = await database;
+    var res = await db.query("ARTICULOS");
+
+    List<Articulo> list =
+        res.isNotEmpty ? res.map((c) => Articulo.fromMap(c)).toList() : [];
+    print(list);
+    return list;
+  }
+
+  deleteClient(int id) async {
+    final db = await database;
+    return db.delete("Client", where: "id = ?", whereArgs: [id]);
+  }
+
+  deleteAll() async {
+    final db = await database;
+    db.rawDelete("Delete * from Client");
+  }
+}
---
+++ lib/Vw_Sincronizar.dart
@@ -0,0 +1,194 @@
+import 'package:testapp/fab_with_icons.dart';
+import 'package:testapp/Database.dart';
+import 'package:testapp/ArticuloModel.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+class Sincronizar extends StatefulWidget {
+  @override
+  SincronizarState createState() {
+    return SincronizarState();
+  }
+}
+
+
+List<Articulo> testArticulos = [
+  Articulo(nombreArticulo: "Raouf"),
+  Articulo(nombreArticulo: "Raouf"),
+  Articulo(nombreArticulo: "Raouf"),
+];
+
+
+Future<List<Articulo>> solicitandoArticulos() async {
+  Map data;
+  final response =
+
+
+  //await http.get('https://jsonplaceholder.typicode.com/posts');
+  await http.post(
+      'http://192.168.0.93:80/inventario.aspx/consultarArticulosJson',
+      headers: {"Content-Type": "application/json"});
+
+
+  if (response.statusCode == 200) {
+    // If the call to the server was successful, parse the JSON
+    data = jsonDecode(response.body);
+    List responseJson = json.decode(data['d']);
+
+    List<Articulo> ListaArticulos = responseJson.map((m) => new Articulo.fromJson(m)).toList();
+    print(ListaArticulos[50].nombreArticulo);
+
+    await ListaArticulos.map((x) async=>(
+        await DBProvider.db.newArticulo(x)
+
+
+    ));
+
+
+
+
+
+    return ListaArticulos;
+
+
+    //return Post.fromJson(json.decode(response.body));
+  } else {
+    // If that call was not successful, throw an error.
+    throw Exception('Failed to load post');
+  }
+}
+
+
+
+// Create a corresponding State class. This class will hold the data related to
+// the form.
+class SincronizarState extends State<Sincronizar> {
+  // Create a global key that will uniquely identify the Form widget and allow
+  // us to validate the form
+  //
+  @override
+  void initState() {
+    DBProvider.db.initDB();
+    super.initState();
+  }
+  // Note: This is a GlobalKey<FormState>, not a GlobalKey<MyCustomFormState>!
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey we created above
+    return Container(
+        alignment: Alignment(0.0, 0.0),
+        constraints: BoxConstraints.expand(width: 290.0, height: 300.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: ButtonTheme(
+                minWidth: 200.0,
+                height: 50.0,
+                child: RaisedButton(
+                  textColor: Colors.white,
+                  padding: const EdgeInsets.all(8.0),
+                  color: Theme.of(context).accentColor,
+                  elevation: 4.0,
+                  splashColor: Colors.blueGrey,
+                  onPressed: () {
+                    solicitandoArticulos();
+
+                  },
+                  child: Text('Sincronizar Datos'),
+                ),
+              ),
+            ),
+          ],
+        ));
+  }
+
+
+
+}
+
+
+class MostrarSincronizacion extends StatefulWidget {
+  @override
+  MostrarSincronizacionState createState() {
+    return MostrarSincronizacionState();
+  }
+}
+
+// Create a corresponding State class. This class will hold the data related to
+// the form.
+class MostrarSincronizacionState extends State<MostrarSincronizacion> {
+  // Create a global key that will uniquely identify the Form widget and allow
+
+  @override
+  Widget build(BuildContext context) {
+    return new FutureBuilder<List<Articulo>>(
+      future: DBProvider.db.getAllArticulo(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return CircularProgressIndicator();
+
+        return ListView.builder(
+          itemCount: snapshot.data.length,
+          itemBuilder: (BuildContext context, int index) {
+            List<Articulo> posts = snapshot.data;
+            print(posts[index].nombreArticulo);
+            return Card(
+
+              child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+
+                  child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        ListTile(
+                          leading: Icon(Icons.album),
+                          title:  Text(posts[index].nombreArticulo, style: TextStyle(fontSize: 18.0),),
+                          subtitle: Center(
+                            child: Column(
+                                crossAxisAlignment:CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Text('Precio:'),
+                                  Text('Strock:'),
+                                  Text('Estado:'),
+
+                                ]
+                            ),),
+                        ),
+                        ButtonTheme.bar( // make buttons use the appropriate styles for cards
+                          child: ButtonBar(
+                            children: <Widget>[
+                              FlatButton(
+                                child: const Text('REVISAR'),
+                                onPressed: () { /* ... */ },
+                              ),
+                              FlatButton(
+                                child: const Text('CONTAR'),
+                                onPressed: () { /* ... */ },
+                              ),
+                            ],
+                          ),
+                        ),
+                      ]
+                  )
+
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+
+
+
+}
+
